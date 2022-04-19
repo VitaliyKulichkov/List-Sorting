77c08b0bb6746d1e64e5c472f27b76155f36d1ce
VitaliyKulichkov <shizgiz1337@gmail.com>
2022-04-19 19:50:31 +0300

    Buble sort

---
+++ BubleSort.py
@@ -0,0 +1,13 @@
+def buble_sort(lst) -> list:
+    for num in range(len(lst)-1, 0, -1):
+        for item in range(num):
+            if lst[item] > lst[item + 1]:
+                lst[item], lst[item + 1] = lst[item + 1], lst[item]
+
+    return lst
+
+lst = [12, 34, 25, 15, 23, 11, 5, 87, 67]
+
+print(f'Исходный массив: {lst}')
+result = buble_sort(lst)
+print(f'Отсортированный массив: {result}')
